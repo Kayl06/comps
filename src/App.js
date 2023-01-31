7112e381381982081f0888ea18cb6b618da01c11
Fedimar Kayl Rabanzo <fedimar.rabanzo@play-asia.com>
2023-01-31 11:41:04 +0800

    Completed Accordion Component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import Accordion from "./components/Accordion";
-import ButtonPage from "./pages/ButtonPage";
+import AccordionPage from "./pages/AccordionPage";
 
 function App() {
-  const items = [
-    {
-      id: '123asd',
-      label: "Can I use React on a project?",
-      content: "Lorem ipmsumewq ferer kwek yes",
-    },
-    {
-      id: 'a23jswe',
-      label: "Can I use React on a bowl?",
-      content: "Lorem ipmsumewq ferer kwek yes",
-    },
-    {
-      id: 'k2lssaw',
-      label: "Can I use React?",
-      content: "Lorem ipmsumewq ferer kwek yes",
-    },
-    {
-      id: 'k21ls',
-      label: "Can I use React on a php?",
-      content: "Lorem ipmsumewq ferer kwek yes",
-    },
-  ];
   return (
     <div>
-      <Accordion items={items} />
+      <AccordionPage />
     </div>
   );
 }
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,33 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: "123asd",
+      label: "Can I use React on a project?",
+      content: "Lorem ipmsumewq ferer kwek yes",
+    },
+    {
+      id: "a23jswe",
+      label: "Can I use React on a bowl?",
+      content: "Lorem ipmsumewq ferer kwek yes",
+    },
+    {
+      id: "k2lssaw",
+      label: "Can I use React?",
+      content: "Lorem ipmsumewq ferer kwek yes",
+    },
+    {
+      id: "k21ls",
+      label: "Can I use React on a php?",
+      content: "Lorem ipmsumewq ferer kwek yes",
+    },
+  ];
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+}
+
+export default AccordionPage;
