cea3d46e5eac617b3b4344a7d5d81d09db0563af
Kayl Rabanzo <frabanzoo@gmail.com>
2023-02-05 01:15:42 +0800

    Navigation updates, added contenxt navigation, applied the popstate and learned about pushState

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    {
-      id: "123asd",
-      label: "Red",
-      value: "red",
-    },
-    {
-      id: "122sd",
-      label: "Blue",
-      value: "blue",
-    },
-    {
-      id: "12342asd",
-      label: "Green",
-      value: "green",
-    },
-  ];
+ 
   return (
-    <div>
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleSelect}
-      />
-    </div>
+    <div></div>
   );
 }
 
