66049fce3b1c6bc83dc9fe81983f2e1d4928868b
Fedimar Kayl Rabanzo <fedimar.rabanzo@play-asia.com>
2023-02-06 17:59:24 +0800

    updates on sidebar style

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,14 @@
 import classNames from "classnames";
 import useNavigation from "../hooks/use-navigation";
 
-function Link({ to, children, className }) {
-  const { navigate } = useNavigation();
+function Link({ to, children, className, activeClassName }) {
+  const { currentPath, navigate } = useNavigation();
 
-  const classes = classNames("text-sky-500", className);
+  const classes = classNames(
+    "text-sky-500", 
+    className,
+    currentPath === to && activeClassName
+  );
 
   const handleClick = (event) => {
     if (event.metaKey || event.ctrlKey) return;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,17 @@ function Sidebar() {
 
   const renderedLinks = links.map((link) => {
     return (
-      <Link className="px-4 py-2 rounded-full hover:bg-sky-400 hover:-translate-y-1 hover:scale-110 duration-200 hover:text-md hover:text-white " key={link.label} to={link.path}>
+      <Link 
+        className="px-4 py-2 mb-2 rounded hover:bg-sky-400 duration-200 hover:text-md hover:text-white" 
+        activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+        key={link.label} 
+        to={link.path}>
         {link.label}
       </Link>
     );
   });
 
-  return <div className="border px-5 py-2 flex flex-col sticky top-0 overflow-y-auto">
+  return <div className="border px-5 py-2 flex flex-col sticky top-0 overflow-y-auto items-start">
     {renderedLinks}
   </div>;
 }
