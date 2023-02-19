5bceeaad41a8d69ce988fcb5fc9212a795b86439
Kayl Rabanzo <frabanzoo@gmail.com>
2023-02-19 23:29:11 +0800

    updates on useReducer topic

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,16 +1,44 @@
+import { useState } from "react";
+import Panel from "../components/Panel";
 import Button from "../components/Button";
-import useCounter from "../hooks/use-counter";
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => setCount(count + 1);
+
+  const decrement = () => setCount(count - 1);
 
   return (
-    <div>
+    <Panel>
       Count is {count}
-      <Button primary rounded onClick={increment}>
-        Increment
-      </Button>
-    </div>
+      <div className="flex flex-row gap-2 ">
+        <Button primary rounded onClick={increment}>
+          Increment
+        </Button>
+        <Button primary rounded onClick={decrement}>
+          Decrement
+        </Button>
+      </div>
+      <form>
+        <label className="mr-2">Add a lot</label>
+        <input
+          type="number"
+          className="bg-gray p-1 m-3 border border-gray-300 rounded"
+        />
+
+        <div className="flex flex-row gap-2">
+          <Button
+            primary
+            outline
+            rounded
+            className="w-40 flex items-center justify-center"
+          >
+            Add it
+          </Button>
+        </div>
+      </form>
+    </Panel>
   );
 }
 
