44a2777bd28a10bb7f2842c3d29e2cdb1850781c
Kayl Rabanzo <frabanzoo@gmail.com>
2023-02-21 23:49:51 +0800

    updates on useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,17 +1,70 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Panel from "../components/Panel";
 import Button from "../components/Button";
 
+const reducer = (state, action) => {
+  console.log(state);
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+      break;
+
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+      break;
+
+    case "CHANGE_VALUE_TO_ADD":
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+      break;
+
+    default:
+      return state;
+      break;
+  }
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
 
-  const increment = () => setCount(count + 1);
+  const increment = () => {
+    dispatch({ type: "INCREMENT" });
+  };
 
-  const decrement = () => setCount(count - 1);
+  const decrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+
+    dispatch({ type: "CHANGE_VALUE_TO_ADD", payload: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // setCount( count + valueToAdd );
+    // setValueToAdd(0);
+  };
 
   return (
     <Panel>
-      Count is {count}
+      Count is {state.count}
       <div className="flex flex-row gap-2 ">
         <Button primary rounded onClick={increment}>
           Increment
@@ -20,10 +73,12 @@ function CounterPage({ initialCount }) {
           Decrement
         </Button>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label className="mr-2">Add a lot</label>
         <input
           type="number"
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
           className="bg-gray p-1 m-3 border border-gray-300 rounded"
         />
 
